f3e63917af8a20f1d9a74297302658d3de671af8
oHqdes <121509181+goodCoder1000@users.noreply.github.com>
2025-09-24 15:46:48 +0000

    did some thingos

---
+++ lesson00_print.py
@@ -0,0 +1,14 @@
+import random
+import time
+
+x = random.randint(1,100)
+y = time.time()
+
+for i in range(1,100000):
+    x = random.randint(1,100)
+    print(x)
+        
+
+print("Hello World")
+print("Age: ", 24)
+print("Sum: ", 6 + 7)
