2b68c4b9815ccd55cfaf3108616f4a3da6031c88
oHqdes <121509181+goodCoder1000@users.noreply.github.com>
2025-09-24 16:17:39 +0000

    type() and \t + finished booleans

--- lesson00_print.py
+++ lesson00_print.py
@@ -4,19 +4,21 @@ import time
 x = random.randint(1,100)
 y = time.time()
 
-for i in range(1,100000):
-    x = random.randint(1,100000)
+for i in range(1,100067):
+    x = random.randint(1,100067)
     print(x)
     if (x == 1):
         print("gg 0.0001% chance")
-        time.sleep(1)
+        time.sleep(0.67)
         
 
 print("Hello World")
 print("Age: ", 24)
 print("Sum: ", 6 + 7)
 
-#print("As \nhe \ncrossed \ntoward the pharmacy at the corner he involuntarily turned his head because of a burst of light that had ricocheted from his temple, and saw, with that quick smile with which we greet a rainbow or a rose, a blindingly white parallelogram of sky being unloaded from the van-a dresser with mirrors across which, as across a cinema screen, passed a flawlessly clear reflection of boughs sliding and swaying not arboreally, but with a human vacillation, produced by the nature of those who were carrying this sky, these boughs, this gliding facade")
+print("\n")
+print("Good \nmorning \npopatlaal")
+print("\tIt is me... \tpopatlaal!")
 
 print("\n")
 # normal integers
@@ -36,4 +38,17 @@ print("\n")
 # ba ba bool (booleans)
 print(True) # 1
 print(False) # 0
-#print(False + True) # 0 + 1 = 1 (True)
+print(False + True) # 0 + 1 = 1 (True)
+print(1 == 1) # True
+print(1 == 0) # False
+print(1 != 1) # False
+print(1 != 0) # True
+print("Hello" == "hello") # False!! CASE SENSITIVE (because in ASCII, the "H" has a different binary than "h")
+print(6 + 7 == 67) # False
+
+# how do I know what data type I'm working with?
+# this is the solution!
+print("\n")
+print(type("hi"))
+print(type(3))
+print(type(3.14159265358979323846264338327950288419716939937510))
