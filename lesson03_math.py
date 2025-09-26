c33351bce041201a7efd43d15d6527457bb63d43
oHqdes <121509181+goodCoder1000@users.noreply.github.com>
2025-09-26 18:00:46 +0000

    math stuff and the most BEAUTIFUL equation ever (yes it equals 67)

---
+++ lesson03_math.py
@@ -0,0 +1,13 @@
+import random
+import time
+
+print("Sum: \t\t", str(41 + 26)) # add + 
+print("Difference: \t", str(69 - 2)) # subtract - 
+print("Product: \t", str(6.7 * 10)) # multiply *
+print("Quotient: \t", str(402 / 6)) # divide / 
+print("Floor Quotient: ", str(403 // 6)) # floor divide //
+print("Modulo: \t", str(6767 % 100)) # modulo % 
+print("Exponent: \t", str(8.18535277187245084594611593922754641018970849310864 ** 2)) # exponentation **
+
+big_equation = ((((round(((6 + 7) * 6) / 7)) ** 7) % 6767) // 67) - 7 # "you cant make an equation using every symbol and only the numbers 6 and 7 that equals 67"
+print("Big equation: \t", big_equation)
