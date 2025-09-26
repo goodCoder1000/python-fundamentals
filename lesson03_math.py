8fd566101e184225ce1103adbf5de93c590807ac
oHqdes <121509181+goodCoder1000@users.noreply.github.com>
2025-09-26 18:17:40 +0000

    did all challenges and make the exponent representation number as small as possible so that the answer still equals exactly 67.000000000

--- lesson03_math.py
+++ lesson03_math.py
@@ -7,7 +7,35 @@ print("Product: \t", str(6.7 * 10)) # multiply *
 print("Quotient: \t", str(402 / 6)) # divide / 
 print("Floor Quotient: ", str(403 // 6)) # floor divide //
 print("Modulo: \t", str(6767 % 100)) # modulo % 
-print("Exponent: \t", str(8.18535277187245084594611593922754641018970849310864 ** 2)) # exponentation **
+print("Exponent: \t", str(8.18535277187245 ** 2)) # exponentation **
 
 big_equation = ((((round(((6 + 7) * 6) / 7)) ** 7) % 6767) // 67) - 7 # "you cant make an equation using every symbol and only the numbers 6 and 7 that equals 67"
 print("Big equation: \t", big_equation)
+
+#--------------------------------------------------------------------------------
+# CHALLENGES
+print("\nChallenges: ")
+
+# 1.
+area_of_rect = 8 * 5
+print("\nArea of Rectangle: ", area_of_rect)
+
+# 2.
+area_of_circle = 3.14 * 7 ** 2
+print("Area of Circle: ", area_of_circle)
+
+# 3.
+cost = 12.99 * 3 + 3.5 * 4
+print("Cost total: ", cost)
+hidden_tax = 14.03 
+cost = cost + hidden_tax
+print("Cost total + tax (not part of challenge): ", cost)
+
+# 4.
+number = random.randint(1, 100)
+if (number % 2 == 0):
+    print("The number " + str(number) + " is even.")
+else:
+    print("The number " + str(number) + " is odd.")
+
+
