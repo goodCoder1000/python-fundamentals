6f4a60f4c89be3e8475562cf20dde0795ae66957
oHqdes <121509181+goodCoder1000@users.noreply.github.com>
2025-09-30 15:54:20 +0000

    did some cool things w/ import math :fire:

---
+++ lesson04_libraries.py
@@ -0,0 +1,11 @@
+import random, time, math
+
+print("\n--= Math Library =--\n")
+
+print("Sqrt: \t\t", math.sqrt(4489))
+print("Round up: \t", math.ceil(66.67))
+print("Round down: \t", math.floor(67.67))
+print("Power: \t\t", math.pow(8.18535277187245, 2))
+print("Pi: \t\t", math.pi)
+print("Euler's Number: ", math.e)
+
