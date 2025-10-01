c44b1983e25718dd35f0e62d57d2be354dbcc259
oHqdes <121509181+goodCoder1000@users.noreply.github.com>
2025-10-01 16:33:41 +0000

    random library added w/ notes also we did the random number generator

--- lesson04_libraries.py
+++ lesson04_libraries.py
@@ -9,3 +9,27 @@ print("Power: \t\t", math.pow(8.18535277187245, 2))
 print("Pi: \t\t", math.pi)
 print("Euler's Number: ", math.e)
 
+#-------------------------------------------------------------------------------
+print("\n--= Random Library =--\n")
+
+# computers can't generate random numbers, so they use...
+# psuedorandom number generator (PRNG) --> not a TRUE random generator
+# rolling a dice --> TRUE random generator
+# to make a random number, computers need to make a bunch of code to make number seem random
+
+# attempt at making a random number without random.randint or random.shuffle
+for i in range(1,1000000):
+    seed = 676.76
+    seed = seed * time.time()
+    seed = seed / 6767
+    seed = math.sqrt(seed)
+    seed = seed * 10000000000
+    seed = seed % 10
+    seed = math.floor(seed)
+    #seed = str(seed)
+    #seed = list(seed)
+    #random.shuffle(seed)
+    print(seed)
+
+
+num = random.randint(1,10)
